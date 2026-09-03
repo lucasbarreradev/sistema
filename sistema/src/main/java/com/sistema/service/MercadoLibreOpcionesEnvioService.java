bf0b70df96ea272968e5fbcd8f2ac74794d7ecde
lucasbarreradev <barreralucas26@gmail.com>
2026-09-03 11:48:58 -0300

    instancia corregida

--- sistema/src/main/java/com/sistema/service/MercadoLibreOpcionesEnvioService.java
+++ sistema/src/main/java/com/sistema/service/MercadoLibreOpcionesEnvioService.java
@@ -6,6 +6,7 @@ import com.sistema.model.Producto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientResponseException;
@@ -26,6 +27,7 @@ public class MercadoLibreOpcionesEnvioService {
     private final Map<String, String> dominiosPorCategoria =
             new java.util.concurrent.ConcurrentHashMap<>();
 
+    @Autowired
     public MercadoLibreOpcionesEnvioService(
             MercadoLibreTokenService tokenService) {
         this(tokenService, crearRestClient());
